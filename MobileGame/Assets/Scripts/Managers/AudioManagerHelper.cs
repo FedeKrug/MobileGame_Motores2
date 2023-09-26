49dcae2c0ff9965f850a14a93593c96a5223525a
FedeKrug <86620474+FedeKrug@users.noreply.github.com>
2023-09-25 23:27:30 -0300

    Menu de Pausa Hecho 😎

---
+++ MobileGame/Assets/Scripts/Managers/AudioManagerHelper.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+public class AudioManagerHelper : MonoBehaviour
+{
+	/// <summary>
+	/// Una clase que tengo para complementar al AudioManager de mi libreria
+	/// </summary>
+	public static AudioManagerHelper instance;
+
+
+	[SerializeField] private Sprite _unmutedVolumeIcon, _mutedVolumeIcon;
+	[SerializeField] private Image _initVolumeIcon;
+
+	public bool mutedVolume;
+
+	private void Awake()
+	{
+		if (instance == null)
+		{
+			instance = this;
+		}
+		else
+		{
+			Destroy(gameObject);
+		}
+	}
+
+	public void ChangeMutedVolumeState()
+	{
+		mutedVolume = !mutedVolume;
+		if (mutedVolume)
+		{
+			//Debug.Log($"<color=yellow>Mute Volume</color>");
+			//Mutear el volumen
+			_initVolumeIcon.sprite = _mutedVolumeIcon;
+		}
+		else
+		{
+			ActiveVolume();
+		}
+	}
+	private void ActiveVolume()
+	{
+		//Debug.Log($"<color=yellow>Unmute Volume</color>");
+			_initVolumeIcon.sprite = _unmutedVolumeIcon;
+
+		//Si el volumen fue muteado, lo desmutea
+	}
+}
--- MobileGame/Assets/Scripts/Player/Movement/JoyController.cs
+++ MobileGame/Assets/Scripts/Player/Movement/JoyController.cs
@@ -7,7 +7,8 @@ public class JoyController : Controller, IDragHandler, IEndDragHandler
 {
     Vector3 dir;
     Vector3 initPos;
-
+    [SerializeField] private Animator _anim;
+    [SerializeField] private string _movingAnimationParameter;
     float maxDistance = 200;
     [SerializeField] RectTransform baseImg = null;
 
@@ -30,13 +31,14 @@ public class JoyController : Controller, IDragHandler, IEndDragHandler
         dir = Vector3.ClampMagnitude((Vector3)eventData.position - initPos, maxDistance);
         transform.position = initPos + dir;
         _playerRef.Moving = true;
-        
+        _anim.SetBool(_movingAnimationParameter, true);
     }
 
     public void OnEndDrag(PointerEventData eventData)
 {
         transform.position = initPos;
         dir = Vector3.zero;
+        _anim.SetBool(_movingAnimationParameter, false);
         _playerRef.Moving = false;
     }
 }
