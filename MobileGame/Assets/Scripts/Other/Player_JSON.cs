a2558bb3fe08cd02fc64fc5609d8281eb57a2d41
FedeKrug <86620474+FedeKrug@users.noreply.github.com>
2023-09-28 23:36:38 -0300

    UPD

---
+++ MobileGame/Assets/Scripts/Other/Player_JSON.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.IO;
+public class Player_JSON : MonoBehaviour
+{
+	[SerializeField] private SaveData _data;
+	private string _path;
+	private void Start()
+	{
+		_path = Application.persistentDataPath + "/data.json";
+	}
+
+	public void Save()
+	{
+		Debug.Log($"<color=yellow>Saved Data</color>");
+		string jsonData = JsonUtility.ToJson(_data, true );
+		File.WriteAllText(_path, jsonData);
+	}
+	[ContextMenu("LoadData")]
+	public void LoadData()
+	{
+		Debug.Log($"<color=yellow>Loaded Data</color>");
+		string jsonLoadData = File.ReadAllText(_path );
+		//JsonUtility.FromJson(); se usa para clases que heredan de Monobehaviour
+		JsonUtility.FromJsonOverwrite(jsonLoadData, _data);
+	}
+}
---
+++ MobileGame/Assets/Scripts/Other/SaveData.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using System;
+using System.IO;
+
+
+[Serializable]
+public class SaveData
+{
+	public int life;
+	public string[] abilities;
+	public int lvl;
+
+	//TODO: Agregar todo lo que necesita ser guardado
+}
